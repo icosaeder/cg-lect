0aa3471fc45c4e9ae31768c7e5a8e85e83db73bf
Kostya Ryabinin <kostya@nulana.com>
2016-05-20 21:24:21 +0500

    updates to complete total refuck

---
+++ Лекции/Практика 4/main.cpp
@@ -0,0 +1,327 @@
+#include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+using namespace std;
+
+GLFWwindow *g_window;
+
+GLuint g_shaderProgram;
+GLint g_uMVP;
+
+class Model
+{
+public:
+    GLuint vbo;
+    GLuint ibo;
+    GLuint vao;
+    GLsizei indexCount;
+};
+
+Model g_model;
+
+GLuint createShader(const GLchar *code, GLenum type)
+{
+    GLuint result = glCreateShader(type);
+
+    glShaderSource(result, 1, &code, NULL);
+    glCompileShader(result);
+
+    GLint compiled;
+    glGetShaderiv(result, GL_COMPILE_STATUS, &compiled);
+
+    if (!compiled)
+    {
+        GLint infoLen = 0;
+        glGetShaderiv(result, GL_INFO_LOG_LENGTH, &infoLen);
+        if (infoLen > 0)
+        {
+            char infoLog[infoLen];
+            glGetShaderInfoLog(result, infoLen, NULL, infoLog);
+            cout << "Shader compilation error" << endl << infoLog << endl;
+        }
+        glDeleteShader(result);
+        return 0;
+    }
+    
+    return result;
+}
+
+GLuint createProgram(GLuint vsh, GLuint fsh)
+{
+    GLuint result = glCreateProgram();
+
+    glAttachShader(result, vsh);
+    glAttachShader(result, fsh);
+    
+    glLinkProgram(result);
+
+    GLint linked;
+    glGetProgramiv(result, GL_LINK_STATUS, &linked);
+    
+    if (!linked)
+    {
+        GLint infoLen = 0;
+        glGetProgramiv(result, GL_INFO_LOG_LENGTH, &infoLen);
+        if (infoLen > 0)
+        {
+            char infoLog[infoLen];
+            glGetProgramInfoLog(result, infoLen, NULL, infoLog);
+            cout << "Shader program linking error" << endl << infoLog << endl;
+        }
+        glDeleteProgram(result);
+        return 0;
+    }
+
+    return result;
+}
+
+bool createShaderProgram()
+{
+    g_shaderProgram = 0;
+
+    const GLchar vsh[] =
+    "#version 330\n"
+    ""
+    "layout(location = 0) in vec3 a_position;"
+    "layout(location = 1) in vec3 a_color;"
+    ""
+    "uniform mat4 u_mvp;"
+    ""
+    "out vec3 v_color;"
+    ""
+    "void main()"
+    "{"
+    "    v_color = a_color;"
+    "    gl_Position = u_mvp * vec4(a_position, 1.0);"
+    "}"
+    ;
+
+    const GLchar fsh[] =
+    "#version 330\n"
+    ""
+    "in vec3 v_color;"
+    ""
+    "layout(location = 0) out vec4 o_color;"
+    ""
+    "void main()"
+    "{"
+    "   o_color = vec4(v_color, 1.0);"
+    "}"
+    ;
+
+    GLuint vertexShader, fragmentShader;
+
+    vertexShader = createShader(vsh, GL_VERTEX_SHADER);
+    fragmentShader = createShader(fsh, GL_FRAGMENT_SHADER);
+
+    g_shaderProgram = createProgram(vertexShader, fragmentShader);
+
+    g_uMVP = glGetUniformLocation(g_shaderProgram, "u_mvp");
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return g_shaderProgram != 0;
+}
+
+bool createModel()
+{
+    const GLfloat vertices[] =
+    {
+        -1.0, -1.0, 1.0, 1.0, 0.0, 0.0,
+        1.0, -1.0, 1.0, 1.0, 0.0, 0.0,
+        1.0, 1.0, 1.0, 1.0, 0.0, 0.0,
+        -1.0, 1.0, 1.0, 1.0, 0.0, 0.0,
+
+        1.0, -1.0, 1.0, 1.0, 1.0, 0.0,
+        1.0, -1.0, -1.0, 1.0, 1.0, 0.0,
+        1.0, 1.0, -1.0, 1.0, 1.0, 0.0,
+        1.0, 1.0, 1.0, 1.0, 1.0, 0.0,
+
+        1.0, 1.0, 1.0, 1.0, 0.0, 1.0,
+        1.0, 1.0, -1.0, 1.0, 0.0, 1.0,
+        -1.0, 1.0, -1.0, 1.0, 0.0, 1.0,
+        -1.0, 1.0, 1.0, 1.0, 0.0, 1.0,
+
+        -1.0, 1.0, 1.0, 0.0, 1.0, 1.0,
+        -1.0, 1.0, -1.0, 0.0, 1.0, 1.0,
+        -1.0, -1.0, -1.0, 0.0, 1.0, 1.0,
+        -1.0, -1.0, 1.0, 0.0, 1.0, 1.0,
+
+        -1.0, -1.0, 1.0, 0.0, 1.0, 0.0,
+        -1.0, -1.0, -1.0, 0.0, 1.0, 0.0,
+        1.0, -1.0, -1.0, 0.0, 1.0, 0.0,
+        1.0, -1.0, 1.0, 0.0, 1.0, 0.0,
+
+        -1.0, -1.0, -1.0, 0.0, 0.0, 1.0,
+        -1.0, 1.0, -1.0, 0.0, 0.0, 1.0,
+        1.0, 1.0, -1.0, 0.0, 0.0, 1.0,
+        1.0, -1.0, -1.0, 0.0, 0.0, 1.0,
+        
+    };
+
+    const GLuint indices[] =
+    {
+        0, 1, 2, 2, 3, 0,
+        4, 5, 6, 6, 7, 4,
+        8, 9, 10, 10, 11, 8,
+        12, 13, 14, 14, 15, 12,
+        16, 17, 18, 18, 19, 16,
+        20, 21, 22, 22, 23, 20
+    };
+
+    glGenBuffers(1, &g_model.vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, g_model.vbo);
+    glBufferData(GL_ARRAY_BUFFER, 24 * 6 * sizeof(GLfloat), vertices, GL_STATIC_DRAW);
+    
+    glGenBuffers(1, &g_model.ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, g_model.ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * 6 * sizeof(GLuint), indices, GL_STATIC_DRAW);
+
+    g_model.indexCount = 6 * 6;
+
+    glGenVertexArrays(1, &g_model.vao);
+    glBindVertexArray(g_model.vao);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (const GLvoid *)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (const GLvoid *)(3 * sizeof(GLfloat)));
+
+    return g_model.vbo != 0 && g_model.ibo != 0 && g_model.vao != 0;
+}
+
+bool init()
+{
+    // Set initial color of color buffer to white.
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+    glEnable(GL_DEPTH_TEST);
+
+    return createShaderProgram() && createModel();
+}
+
+void reshape(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void draw()
+{
+    // Clear color buffer.
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glUseProgram(g_shaderProgram);
+    glBindBuffer(GL_ARRAY_BUFFER, g_model.vbo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, g_model.ibo);
+    glBindVertexArray(g_model.vao);
+
+    const GLfloat mvp[] =
+    {
+        1.708748f, -1.478188f, -0.360884f, -0.353738f,
+        0.000000f, 1.208897f, -0.883250f, -0.865760f,
+        -1.707388f, -1.479366f, -0.361171f, -0.354019f,
+        0.000000f, 0.000000f, 4.898990f, 5.000000f
+    };
+
+    glUniformMatrix4fv(g_uMVP, 1, GL_FALSE, mvp);
+
+    glDrawElements(GL_TRIANGLES, g_model.indexCount, GL_UNSIGNED_INT, NULL);
+}
+
+void cleanup()
+{
+    if (g_shaderProgram != 0)
+        glDeleteProgram(g_shaderProgram);
+    if (g_model.vbo != 0)
+        glDeleteBuffers(1, &g_model.vbo);
+    if (g_model.ibo != 0)
+        glDeleteBuffers(1, &g_model.ibo);
+    if (g_model.vao != 0)
+        glDeleteVertexArrays(1, &g_model.vao);
+}
+
+bool initOpenGL()
+{
+    // Initialize GLFW functions.
+    if (!glfwInit())
+    {
+        cout << "Failed to initialize GLFW" << endl;
+        return false;
+    }
+
+    // Request OpenGL 3.3 without obsoleted functions.
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Create window.
+    g_window = glfwCreateWindow(800, 600, "OpenGL Test", NULL, NULL);
+    if (g_window == NULL)
+    {
+        cout << "Failed to open GLFW window" << endl;
+        glfwTerminate();
+        return false;
+    }
+
+    // Initialize OpenGL context with.
+    glfwMakeContextCurrent(g_window);
+
+    // Set internal GLEW variable to activate OpenGL core profile.
+    glewExperimental = true;
+
+    // Initialize GLEW functions.
+    if (glewInit() != GLEW_OK) 
+    {
+        cout << "Failed to initialize GLEW" << endl;
+        return false;
+    }
+
+    // Ensure we can capture the escape key being pressed.
+    glfwSetInputMode(g_window, GLFW_STICKY_KEYS, GL_TRUE);
+
+    // Set callback for framebuffer resizing event.
+    glfwSetFramebufferSizeCallback(g_window, reshape);
+
+    return true;
+}
+
+void tearDownOpenGL()
+{
+    // Terminate GLFW.
+    glfwTerminate();
+}
+
+int main()
+{
+    // Initialize OpenGL
+    if (!initOpenGL())
+        return -1;
+
+    // Initialize graphical resources.
+    bool isOk = init();
+
+    if (isOk)
+    {
+        // Main loop until window closed or escape pressed.
+        while (glfwGetKey(g_window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(g_window) == 0)
+        {
+            // Draw scene.
+            draw();
+
+            // Swap buffers.
+            glfwSwapBuffers(g_window);
+            // Poll window events.
+            glfwPollEvents();
+        }
+    }
+
+    // Cleanup graphical resources.
+    cleanup();
+
+    // Tear down OpenGL.
+    tearDownOpenGL();
+
+    return isOk ? 0 : -1;
+}
